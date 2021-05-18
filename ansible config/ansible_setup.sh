a61f629fd12d79082737127f0322fe5f561295a9
SydAsif <sydasif78@gmail.com>
2021-05-18 21:20:59 +0500

    Update and some new configs

--- ansible config/ansible_setup.sh
+++ ansible config/ansible_setup.sh
@@ -1,36 +1,42 @@
-#1. Have a "username" and "sudo" level access
-#2. Setup passwordless keys
-#3. Enable SSH on remote hosts:- 
-sudo apt install openssh-server
-
 #INSTALL AND CONFIG
 #Remove sudo when use in NetworkAutomation docker in GNS3
-
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
 
+#Installing Ansible with pip
+sudo apt-get update
+sudo apt-get install python3 python3-pip git
+pip3 install ansible
+
 #confirm working by running below commands: 
 ansible --version
 ansible localhost -m ping
 
 ######## PASSWORD LESS KEYS ###########
-#Create SSH key on ansible server
-ssh-keygen
-#accept the defaults
-#list the keys to verify
-ls .ssh
+#Create SSH key on ansible server and accept the defaults
+ssh-keygen -t rsa
+sudo apt-get install openssh-server -y
 
-#copy the key to other machines
+#Now copy the keys to your host
+ssh-copy-id localhost
+#Now you are allow to connect to your localhost via SSH
+ssh localhost
+#accept fingerprint
+exit
+# it’s time to run our first Ansible command
+ansible all -i "localhost," -m shell -a 'echo Ansible is fun'
+#list the keys to verify and copy the key to other machines
+ls .ssh
 ssh-copy-id -i .ssh/id_rsa.pub ubuntu@192.168.100.10
 ssh-copy-id -i .ssh/id_rsa.pub centos@192.168.100.20
 #accept fingerprint
 #note if refused, run:
 sudo apt install openssh-server # replace apt with yum in centos7
 
-#Now configure ubuntu so that it doesnt require a password
+#Now configure ubuntu so that it doesn't require a password
 ssh ubuntu@192.168.100.10
 sudo visudo
 #go to bottom of file add this line:
@@ -51,11 +57,10 @@ centos ALL=(ALL) NOPASSWD: ALL
 
 #Create ansible.cfg file docker 
 sudo nano ansible.cfg
-#Copy and paste
 [defaults]
 inventory =./hosts
-host_key_checking =False
-deprecation_warnings=False
+host_key_checking =False # for network dvice
+deprecation_warnings=False # for older version of Python
 timeout=30
 
 ###### INVENTORY #######
@@ -67,7 +72,7 @@ ubuntu ansible_host=192.168.100.10 ansible_user=ubuntu
 [centos7]
 centos ansible_host=192.168.100.20 ansible_user=centos
 
-## Adhoc command to checking connection
+## Adhoc command to check connection
 ansible -m ping all
 ansible -m raw -a '/usr/bin/uptime' all
 ansible -m shell -a 'python3 -V' ubuntu20
